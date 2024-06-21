7a4de49285e1a9ce601390475bdfb5fc05049b03
Parasabhinav <46993013+Parasabhinav@users.noreply.github.com>
2024-06-21 16:27:12 +0530

    Delete commonResources directory

--- commonResources/src/main/java/com/example/commonresources/AudioUrlConstant.kt
+++
@@ -1,8 +0,0 @@
-package com.example.commonresources
-
-object AudioUrlConstant {
-
-    const val AUDIO_URL_TRIM = "http://ankit.parastechnologies.in/muselink/uploads/trim-audio/"
-    const val AUDIO_URL_FULL = "http://ankit.parastechnologies.in/muselink/uploads/full-audio/"
-
-}
